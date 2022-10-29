01d84113eea13c0b8c8e27ba1d8a512702a0660d
Bang Pateng <38981255+bangpateng@users.noreply.github.com>
2022-10-29 18:10:20 +0700

    Update deinfra.sh

--- deinfra.sh
+++ deinfra.sh
@@ -23,7 +23,7 @@ sleep 2
 
 echo -e "\e[1m\e[32m1. Updating packages... \e[0m" && sleep 1
 # update
-sudo apt update -y && apt instal jq && sudo apt install apt-transport-https ca-certificates curl software-properties-common -y && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" && sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin && sudo apt install docker-compose
+sudo apt update -y && sudo apt install jq && sudo apt install apt-transport-https ca-certificates curl software-properties-common -y && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" && sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin && sudo apt install docker-compose
 
 echo -e "\e[1m\e[32m2. Pull Docker... \e[0m" && sleep 1
 # pull
